f7c5a76604e7a2f15887d77c6f5e40a51003e5f9
Erik Örjehag <erik@orjehag.se>
2022-03-05 21:08:57 +0000

    test on hardware

--- ros/jeff.py
+++ ros/jeff.py
@@ -1,55 +1,85 @@
 
 
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+import microphone
 
-servoPin = 12
+def servo():
+  servoPin = 13
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servoPin, GPIO.OUT)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(servoPin, GPIO.OUT)
 
-pwm=GPIO.PWM(servoPin, 50)
-pwm.start(0)
+  p = GPIO.PWM(servoPin, 50)
 
+  p.start(2.5) # Initialization
+  try:
+    while True:
+      p.ChangeDutyCycle(5)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(7.5)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(10)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(12.5)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(10)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(7.5)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(5)
+      time.sleep(0.5)
+      p.ChangeDutyCycle(2.5)
+      time.sleep(0.5)
+  except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
 
+def ledsAndBuzzer():
+  ledPinGreen = 16
+  ledPinRed = 25
+  ledPinYellow = 12
+  buzzPin = 21
+  btnPinRed = 23
+  btnPinGreen = 24
 
-def setAngle(angle):
-    duty = angle / 18 + 3
-    GPIO.output(servoPin, True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(servoPin, False)
-    pwm.ChangeDutyCycle(duty)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setwarnings(False)
 
-class Snuffer:
-    def __init__(self, port):
-        self.port = port
+  GPIO.setup(ledPinGreen, GPIO.OUT)
+  GPIO.setup(ledPinYellow, GPIO.OUT)
+  GPIO.setup(ledPinRed, GPIO.OUT)
+  GPIO.setup(buzzPin, GPIO.OUT)
+  GPIO.setup(btnPinRed, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(btnPinGreen, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def snuff(self):
-        setAngle(10)
+  start = time.time()
 
-    def unSnuff(self):
-        setAngle(170)
+  try:
+    while True:
+      
+      if int(time.time() - start) % 2 == 0:
+        GPIO.output(ledPinYellow, GPIO.HIGH)
+        GPIO.output(buzzPin, GPIO.LOW)
+      else:
+        GPIO.output(ledPinYellow, GPIO.LOW)
+        GPIO.output(buzzPin, GPIO.HIGH)
+      
+      if GPIO.input(btnPinRed) == GPIO.LOW:
+        GPIO.output(ledPinRed, GPIO.HIGH)
+      else:
+        GPIO.output(ledPinRed, GPIO.LOW)
+      
+      if GPIO.input(btnPinGreen) == GPIO.LOW:
+        GPIO.output(ledPinGreen, GPIO.HIGH)
+      else:
+        GPIO.output(ledPinGreen, GPIO.LOW)
 
+  except KeyboardInterrupt:
+    GPIO.cleanup()
 
-snuffer = Snuffer(port=4)
-snuffer.snuff()
+if __name__ == "__main__":
+  microphone.block_until_start_signal()
+  ledsAndBuzzer()
+  #servo()
 
-# setAngle(0)
-
-# sleep(2)
-
-
-# for vinkel in range(0,150,30):
-#     setAngle(vinkel)
-#     sleep(1)
-
-
-# setAngle(0)
-# sleep(1)
-# setAngle(90)
-# sleep(3)
-# setAngle(110)
-
-pwm.stop()
-GPIO.cleanup()
